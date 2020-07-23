a0c57f9a21ffdde667138092581b629fe5a4c72e
Will Francis <wbf7@duke.edu>
2020-07-23 14:13:38 -0500

    progress

---
+++ auth.js
@@ -0,0 +1,40 @@
+function getUser(){
+    const token = "Bearer " + localStorage.token
+    return $.ajax({
+        url: 'https://dev-will.auth0.com/userinfo',
+        type: "GET",
+        headers: {
+            'Authorization': token
+        },
+        success: (response) => {
+            // console.log(response)
+            return response
+        },
+        error: (error) => {
+            login()
+        }
+    });
+}
+
+function logout(webAuth){
+    localStorage.clear()
+    console.log(localStorage)
+    webAuth.logout({
+        returnTo: 'https://recipebox-willbfrancis.herokuapp.com/home.html',
+        client_id: 'x7YM3jmiJVLNLDJSIccy1kw5RKdWC6AH'
+    });
+}
+
+function login(webAuth){
+    webAuth.authorize({
+    });
+}
+
+// function login(webAuth, redirectUri){
+//     webAuth.authorize({redirectUri
+//     });
+// }
+
+function isExpired(token){
+    console.log(token)
+}
